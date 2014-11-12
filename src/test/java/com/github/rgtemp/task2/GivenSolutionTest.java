40f2442bfe1c24bdc3d385eeabfdd6c34a7d1d2a
rg-shade <rg-shade@yandex.com>
2014-11-12 20:35:23 +0100

    Added example matrix.

--- src/test/java/com/github/rgtemp/task2/GivenSolutionTest.java
+++ src/test/java/com/github/rgtemp/task2/GivenSolutionTest.java
@@ -10,25 +10,40 @@ import static org.hamcrest.Matchers.is;
 
 public class GivenSolutionTest {
 	
-	private static Function<int[], Integer> codilityRuner;
+	private static Function<int[][], Integer> codilityRuner;
 	
-	protected int[] getExampleInput() {
-		throw new RuntimeException("Implement me");
-//		return ;
+	protected int[][] getExampleInput() {
+//		A[0][0] = 5    A[0][1] = 4    A[0][2] = 4
+//		A[1][0] = 4    A[1][1] = 3    A[1][2] = 4
+//		A[2][0] = 3    A[2][1] = 2    A[2][2] = 4
+//		A[3][0] = 2    A[3][1] = 2    A[3][2] = 2
+//		A[4][0] = 3    A[4][1] = 3    A[4][2] = 4
+//		A[5][0] = 1    A[5][1] = 4    A[5][2] = 4
+//		A[6][0] = 4    A[6][1] = 1    A[6][2] = 1
+		int[][] input = {
+				{5, 4, 4,},
+				{4, 3, 4,},
+				{3, 2, 4,},
+				{2, 2, 2,},
+				{3, 3, 4,},
+				{1, 4, 4,},
+				{4, 1, 1,},
+				};
+		
+		return input;
 	}
 	
 	@BeforeClass
 	public static void setUpClass() {
-		throw new RuntimeException("Implement me");
-//		codilityRuner = new EditedSolution();
+		codilityRuner = new GivenSolution();
 	}
 	
 	@Test public void thatTheGivenExampleWorks() {
-//		int in = getExampleInput;
-//		int actual = codilityRuner.apply(in);
-//		int expected = 4;
-//		String reason = String.format("Task description states that %d should return %d", in, expected);
-//		assertThat(reason, actual, is(expected));
+		int[][] in = getExampleInput();
+		int actual = codilityRuner.apply(in);
+		int expected = 5;
+		String reason = String.format("Task description states that %d should return %d", in, expected);
+		assertThat(reason, actual, is(expected));
 	}
 	
 }
